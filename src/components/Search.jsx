7973033ddde9a099a49783c141d83c25f3956387
Zane Reid <zanecreid@hotmail.com>
2023-04-27 15:10:01 +0100

    Buggy still need fixing... geolocation for city search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,12 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Geocode from "react-geocode";
+import SearchForm from "./SearchForm";
 
 function Search() {
   const [weatherData, setWeatherData] = useState(null);
+  const [lat, setLat] = useState();
+  const [lon, setLon] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,8 +18,7 @@ function Search() {
 
       try {
         const response = await axios.get(
-          "http://api.weatherbit.io/v2.0/current",
-          { params }
+          "http://api.weatherbit.io/v2.0/current"
         );
         setWeatherData(response.data);
       } catch (err) {
@@ -26,25 +29,54 @@ function Search() {
     fetchData();
   }, []);
 
+  const handleSearch = async (query) => {
+    try {
+      const response = await axios.get(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=AIzaSyBIyET3ksgL5bSzBl8YhzW-3kpdbW_7VmQ`
+      );
+      const result = response.data.result[0];
+      const { lat, lng } = result.geometry.location;
+      setLat(lat);
+      setLon(lng);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const getIconUrl = (iconCode) => {
     return `https://www.weatherbit.io/static/img/icons/${iconCode}.png`;
   };
 
   return (
     <div>
-      {weatherData ? (
-        <>
-          <h1>Current Weather</h1>
-          <p>Temperature: {weatherData.data[0].temp}</p>
-          <p>Weather Description: {weatherData.data[0].weather.description}</p>
-          <p>City: {weatherData.data[0].city_name}</p>
-          <img src={getIconUrl(weatherData.data[0].weather.icon)} alt="" />
-        </>
-      ) : (
-        <div>Loading...</div>
-      )}
+      <div>
+        {weatherData ? (
+          <>
+            <h1>Current Weather</h1>
+            <p>Temperature: {weatherData.data[0].temp}</p>
+            <p>
+              Weather Description: {weatherData.data[0].weather.description}
+            </p>
+            <p>City: {weatherData.data[0].city_name}</p>
+            <img src={getIconUrl(weatherData.data[0].weather.icon)} alt="" />
+          </>
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
+      <div>
+        <h1>Search for a city</h1>
+        <SearchForm onSearch={handleSearch} />
+        {location && (
+          <p>
+            The latitude is {lat} and the longitude is {lon}.
+          </p>
+        )}
+      </div>
     </div>
   );
 }
+
+Geocode.setApiKey("AIzaSyBIyET3ksgL5bSzBl8YhzW-3kpdbW_7VmQ");
 
 export default Search;
---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const SearchForm = ({ onSearch }) => {
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSearch(query);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter a city name"
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+export default SearchForm;
