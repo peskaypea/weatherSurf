72cec9ea0ab5219ef8e8ffdb91a11e3b1a844d70
Zane Reid <zanecreid@hotmail.com>
2023-04-29 10:31:03 +0100

    added in Proptypes Validation to the WeatherCard

--- src/components/Weather/WeatherCard.jsx
+++ src/components/Weather/WeatherCard.jsx
@@ -1,21 +1,27 @@
+import PropTypes from "prop-types";
+
 const WeatherCard = ({
   cityTemperature,
   cityDescription,
   cityName,
   weatherImg,
 }) => {
-  const getIconUrl = (iconCode) => {
-    return `https://www.weatherbit.io/static/img/icons/${iconCode}.png`;
-  };
   return (
     <div>
       <h1>Current Weather</h1>
       <p>Temperature: {cityTemperature}</p>
       <p>Weather Description: {cityDescription}</p>
       <p>City: {cityName}</p>
-      <img src={weatherImg} alt="" />
+      <img src={weatherImg} alt="weatherImage" />
     </div>
   );
+};
+
+WeatherCard.propTypes = {
+  cityTemperature: PropTypes.number.isRequired,
+  cityDescription: PropTypes.string.isRequired,
+  cityName: PropTypes.string.isRequired,
+  weatherImg: PropTypes.string.isRequired,
 };
 
 export default WeatherCard;
